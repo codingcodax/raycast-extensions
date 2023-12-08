d396973e3d5bae00298cec1b04a36ca062298e94
Alexis Guzman <codingcodax@gmail.com>
2023-12-08 12:36:59 -0600

    feat: setup team time template

---
+++ team-time/src/index.ts
@@ -0,0 +1,28 @@
+import { Clipboard, environment, LaunchType, Toast, updateCommandMetadata } from "@raycast/api";
+
+const command = async () => {
+  const now = new Date();
+
+  const mexico = now.toLocaleString(undefined, { timeZone: "America/Mexico_City", timeStyle: "short" });
+  const venezuela = now.toLocaleString(undefined, { timeZone: "America/Caracas", timeStyle: "short" });
+  const newYork = now.toLocaleString(undefined, { timeZone: "America/New_York", timeStyle: "short" });
+
+  const subtitle = `🇲🇽 ${mexico}    🇺🇸 ${newYork}  🇻🇪 ${venezuela}`;
+  await updateCommandMetadata({ subtitle });
+
+  if (environment.launchType === LaunchType.UserInitiated) {
+    const toast = new Toast({
+      style: Toast.Style.Success,
+      title: "Refreshed!",
+      message: subtitle,
+    });
+    toast.primaryAction = {
+      title: "Copy to Clipboard",
+      shortcut: { modifiers: ["cmd", "shift"], key: "c" },
+      onAction: () => Clipboard.copy(subtitle),
+    };
+    await toast.show();
+  }
+};
+
+export default command;
