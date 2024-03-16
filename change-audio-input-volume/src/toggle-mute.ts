50b1813a9497e05948ace3c067a4b2bec2d85736
Alexis Guzman <codingcodax@gmail.com>
2024-03-16 17:20:48 -0600

    feat(commands): create toggle mute

---
+++ change-audio-input-volume/src/toggle-mute.ts
@@ -0,0 +1,20 @@
+import { LocalStorage } from "@raycast/api";
+import { runAppleScript } from "run-applescript";
+
+export default async function toggleMute() {
+  const currentVolume = await runAppleScript('return input volume of (get volume settings)');
+
+  const audioInputIsMuted = currentVolume.trim() === "0";
+
+  if (currentVolume.trim() !== "0" && currentVolume.trim() !== "100") {
+    await LocalStorage.setItem("audioInputVolume", currentVolume);
+  }
+
+  if (!audioInputIsMuted) {
+    await runAppleScript(`set volume input volume 0`);
+  } else {
+    const previousVolume = await LocalStorage.getItem<string>("audioInputVolume");
+    await runAppleScript(`set volume input volume ${previousVolume || "100"}`);
+    await LocalStorage.removeItem("audioInputVolume");
+  }
+}
